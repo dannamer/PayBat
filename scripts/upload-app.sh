486e3a747cb009d49d3e1e5d211b983a43c9193e
Azreil-OFD <fantom2413@gmail.com>
2025-01-31 06:11:04 +0500

    fix

--- scripts/upload-app.sh
+++ scripts/upload-app.sh
@@ -3,8 +3,6 @@
 mkdir -p .tmp/saved/
 cp -r .git .tmp/saved/
 cd .tmp/saved/
-git revert --no-edit .
-git checkout main
 RELEASE_BRANCHES=$(git branch -r | grep -E 'origin/.*-release$' | sed 's/origin\///')
 mkdir -p ../apps/
 
@@ -21,7 +19,6 @@ for branch in $RELEASE_BRANCHES; do
 done
 
 cd ../..
-rm -rf ./.tmp/saved/
 mkdir -p ./services/
 
 for app_dir in ./.tmp/apps/*; do
@@ -33,5 +30,3 @@ for app_dir in ./.tmp/apps/*; do
 
   mv "$app_dir" ./services/
 done
-
-rm -rf ./.tmp/apps/
